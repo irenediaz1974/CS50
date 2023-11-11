fb27a913e74febb9f389fe2363e1bb465b9ec070
Irene Diaz <irenediazcuba@gmail.com>
2023-11-11 07:10:54 -0500

    0411

--- Week6/dna/dna.py
+++ Week6/dna/dna.py
@@ -3,18 +3,59 @@ import sys
 
 
 def main():
-
     # TODO: Check for command-line usage
-
-    # TODO: Read database file into a variable
-    
-    # TODO: Read DNA sequence file into a variable
-
+    database_name, sequence_name = check_arguments()
+    # TODO: Read database file into a list of dictionaries
+    humans = open_database(database_name)
+    # TODO: Read DNA sequence file into a string
+    sequences = open_sequence(sequence_name)
     # TODO: Find longest match of each STR in DNA sequence
+    # Creando un diccionario iterando a traves de otro diccionario con clave key
+    values = dict()
+    for key in humans[0].keys():
+        if key != "name":
+            values[key] = longest_match(sequences, key)
 
     # TODO: Check database for matching profiles
 
+    for human in humans:
+        match = True
+        for key, value in values.items():
+            if human.get(key) != str(value):
+                match = False
+                break
+        if match:
+            print(human["name"])
+            break
+    if match == False:
+        print("No match")
+
     return
+
+
+def check_arguments():
+    while True:
+        try:
+            database_name = sys.argv[1]
+            sequence_name = sys.argv[2]
+            return database_name, sequence_name
+        except IndexError:
+            print("Usage: python dna.py data.csv sequence.txt")
+
+
+def open_database(d):
+    with open(d, "r") as file:
+        reader = csv.DictReader(file)
+        humans = []
+        for row in reader:
+            humans.append(row)
+        return humans
+
+
+def open_sequence(s):
+    with open(s, "r") as file:
+        sequence = file.read()
+        return sequence
 
 
 def longest_match(sequence, subsequence):
@@ -27,7 +68,6 @@ def longest_match(sequence, subsequence):
 
     # Check each character in sequence for most consecutive runs of subsequence
     for i in range(sequence_length):
-
         # Initialize count of consecutive runs
         count = 0
 
@@ -35,7 +75,6 @@ def longest_match(sequence, subsequence):
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
             # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
@@ -43,11 +82,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
--- Week6/world-cup/tournament.py
+++ Week6/world-cup/tournament.py
@@ -9,26 +9,28 @@ N = 1000
 
 
 def main():
-
     # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
     # TODO: Read teams into memory from file
-    filename=sys.argv[1]
+    filename = sys.argv[1]
     with open(filename) as f:
-        reader=csv.DictReader(f)
+        reader = csv.DictReader(f)
         for team in reader:
-            team["rating"]= int(team["rating"])
+            team["rating"] = int(team["rating"])
             teams.append(team)
+
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-        winner= simulate_tournament(teams)
+    # winner = simulate_tournament(teams)
+    for i in range(N):
+        winner = simulate_tournament(teams)
         if winner in counts:
-            counts[winner]+=1
+            counts[winner] += 1
         else:
-            counts[winner]=1
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -60,9 +62,10 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    while len(teams)>1:
-        teams=simulate_round(teams)
-        return teams[0]["team"]
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
+
 
 if __name__ == "__main__":
     main()
