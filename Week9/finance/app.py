38787ff32b8c71af040f3a7f688072d31eae020d
Irene Diaz <irenediazcuba@gmail.com>
2023-12-05 04:19:26 -0500

    ok

--- Week9/finance/app.py
+++ Week9/finance/app.py
@@ -34,8 +34,15 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-    return apology("TODO")
+    """Show portfolio of stocks """
+    result = db.execute("SELECT * FROM stocks WHERE user_id= ? ", session["user_id"])
+    # Add price and name actual to the user stocks
+    for row in result:
+        symbol_dict = lookup(row["symbol"])
+        row.update(symbol_dict)
+
+    return render_template("index.html", rows=result)
+    
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -125,28 +132,25 @@ def register():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-        # Ensure password was submitted
-        if not request.form.get("password"):
-            return apology("must provide password", 403)
-        # Ensure password confirmation was submitted
-        if not request.form.get("confirmation") or request.form.get("confirmation")!= request.form.get("password"):
+        dict={"username": request.form.get("username"), "password":request.form.get("password"), "confirmation": request.form.get("confirmation")}
+        for key in dict:
+            if not dict[key]:
+                 return apology(f"must provide {key}", 403)
+        if request.form.get("confirmation")!= request.form.get("password"):
             return apology("password don't match", 400)
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Ensure username not exists in database
+        if len(rows) >= 1:
+            flash('Username is not available')
+            return redirect("/register")
         else:
-            # Query database for username
-            rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-            # Ensure username not exists in database
-            if len(rows) >= 1:
-                flash('Username is not available')
-                return redirect("/register")
-            else:
-                # Insert username and password into database
-                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
-                last_id = db.execute("SELECT last_insert_rowid()")[0]["last_insert_rowid()"]
-                session["user_id"] = last_id
-                # abrir sesion y mostrar portfolio de stoks por user
-                redirect(url_for('index'))
+            # Insert username and password into database
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
+            last_id = db.execute("SELECT last_insert_rowid()")[0]["last_insert_rowid()"]
+            session["user_id"] = last_id
+            # abrir sesion y mostrar portfolio de stoks por user index function
+            return redirect(url_for('index'))
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
